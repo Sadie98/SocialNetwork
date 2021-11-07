407b9e78d0f28022afe652f75cf840186eeba1c3
Sadie <avdeewa.ul@yandex.ru>
2021-11-07 17:56:38 +0300

    New posts, layout

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import s from './Posts.module.css';
+import DialogsBackground from './../assets/images/dialogsBackground.jpg'
 
 const Posts = () => {
     return (
-        <div>Posts</div>
+        <div className={s.posts}>
+            <div className={s.background}>
+                <img src={DialogsBackground}/>
+            </div>
+            <div className={s.new_post}>
+                <textarea className={s.create_textarea}/>
+                <button className={s.create_button}>Create post</button>
+            </div>
+            <div class={s.posts_list}></div>
+        </div>
     )
 }
 
